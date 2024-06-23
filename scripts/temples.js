11a8a296ef5af5c503df242271daa126e8d7b53c
tcurtis1 <tcurtis1@gmail.com>
2024-06-22 21:03:39 -0600

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,13 +1,13 @@
-// Store the selected elements that we are going to use.
+// Grab the dom elements we are planning to dynamically update below
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
-// Add a click event listener to the hamburger button and use a callback function that toggles the list element's list of classes.
+// Register events that we want to make changes when it happens
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
 });
 
 // Set the last updated date
-const lastUpdated = document.getElementById('last-updated');
-lastUpdated.textContent = document.lastModified;
+// const lastUpdated = document.getElementById('last-updated');
+// lastUpdated.textContent = document.lastModified;
